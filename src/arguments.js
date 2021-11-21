0b6c621c41a5a1a8e78bcc7b96d38c1de55a137e
Luda <luda.tamb@mail.ru>
2021-11-21 23:40:01 +0300

    fix: change validate function

--- src/arguments.js
+++ src/arguments.js
@@ -1,44 +1,30 @@
 const fs = require('fs')
+let arr = process.argv.slice(2)
 
-let arr = process.argv.slice(2);
-
-if (arr.length > new Set(arr).size) {
-    process.stderr.write('Some arguments are duplicated')
-    process.exit(-1)
-}
-
-let groups = {}
-for (let i = 0; i < arr.length; i += 2) {
-    groups[arr[i]] = arr[i + 1]
-}
-
-let encodingGroup = groups['-c'].split('-');
-let inputFile = groups['-i']
-let outputFile = groups['-o']
-
-if(!groups.hasOwnProperty('-c') || groups['-c'] === undefined) {
-    process.stderr.write('Please enter valid config option')
-    process.exit(-1)
-}
-
-
-if(inputFile) {
-    try {
-        fs.accessSync(inputFile, fs.constants.F_OK | fs.constants.R_OK)
-    } 
-
-    catch(err) {
-        if(err) {
-            process.stderr.write(`Input file ${err.code === 'ENOENT' ? 'does not exist' : 'cannot be read'}.`)
-            process.exit(-1)
+const getDublicatedArg = (arr) => {
+    let copyOfArr = arr.slice().sort()
+    
+    for(let i = 0; i < copyOfArr.length; i += 1) {
+        if(copyOfArr[i] === copyOfArr[i + 1]) {
+            return copyOfArr[i]
         }
     }
+}
 
-    if (fs.lstatSync(inputFile).isDirectory()) {
-        process.stderr.write('Input file cannot be a directory.')
+const isArgDuplicated = (arr) => {
+    if (getDublicatedArg(arr)) {
+        process.stderr.write(`Error: You provided ${getDublicatedArg(arr)} argument more than once`)
         process.exit(-1)
     }
-}   
+}
+
+isArgDuplicated(arr)
+
+let groups = {}
+let encodingGroup
+let inputFile 
+let outputFile
+
 
 const validateEncodingItem = (item) => {
     if(item.length === 1 && item === 'A') return true
@@ -51,14 +37,56 @@ const validateEncodingItem = (item) => {
     return false
 }
 
-for(let i = 0; i < encodingGroup.length; i += 1) {
-    if(!validateEncodingItem(encodingGroup[i])) {
+
+if(arr.length >= 2) {
+    for (let i = 0; i < arr.length; i += 2) {
+        groups[arr[i]] = arr[i + 1]
+    }
+    
+    encodingGroup = groups['-c'].split('-');
+    inputFile = groups['-i']
+    outputFile = groups['-o']
+
+    if(!groups.hasOwnProperty('-c') || groups['-c'] === undefined) {
         process.stderr.write('Please enter valid config option')
         process.exit(-1)
     }
+
+    for(let i = 0; i < encodingGroup.length; i += 1) {
+        if(!validateEncodingItem(encodingGroup[i])) {
+            process.stderr.write('Please enter valid config option')
+            process.exit(-1)
+        }
+    }
 }
 
+const validateInputFile = (file) => {
+    try {
+        fs.accessSync(file, fs.constants.F_OK | fs.constants.R_OK)
+    } 
+
+    catch(err) {
+        if(err) {
+            process.stderr.write(`Input file does not exist`)
+            //process.exit(-1)
+        }
+    }
+
+    // if (fs.lstatSync(inputFile.tempFilePath).isDirectory()) {
+    //     process.stderr.write('Input file cannot be a directory.')
+    //     process.exit(-1)
+    // }
+}
+
+validateInputFile(inputFile)
+  
+
+
 module.exports = {
+    getDublicatedArg,
+    isArgDuplicated,
+    validateEncodingItem,
+    validateInputFile,
     encodingGroup,
     inputFile,
     outputFile
